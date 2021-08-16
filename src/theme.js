666182b0bf76298418266223afae8d1cff3c9f92
Tutul <tutulnahid@gmail.com>
2021-08-17 01:35:35 +0600

    updated theme

--- src/theme.js
+++ src/theme.js
@@ -9,6 +9,37 @@ const theme = createTheme({
     background: {
       default: "#fdfdfd",
     },
+    green: {
+      main: "rgb(0, 82, 73)",
+      dark: "rgb(0, 123, 85)",
+      light: "rgb(200, 250, 205)",
+      darker: "rgb(0 171 85)",
+    },
+    blue: {
+      main: "rgb(4, 41, 122)",
+      dark: "rgb(12, 83, 183)",
+      light: "rgb(208, 242, 255)",
+    },
+    yellow: {
+      main: "rgb(122, 79, 1)",
+      dark: "rgb(183, 129, 3)",
+      light: "rgb(255, 247, 205)",
+    },
+    maroon: {
+      main: "rgb(122, 12, 46)",
+      dark: "rgb(183, 33, 54)",
+      light: "rgb(255, 231, 217)",
+    },
+    gray: {
+      main: "rgb(99, 115, 129)",
+      light: "rgba(145, 158, 171, 0.08)",
+      lighter: "rgb(244, 246, 248)",
+    },
+    mixins: {
+      toolbar: {
+        minHeight: 64,
+      },
+    },
   },
 });
 
