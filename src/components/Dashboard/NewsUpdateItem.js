cbf5444b1081bad7e29e42d6374f5483a2f8f4fc
Tutul <tutulnahid@gmail.com>
2021-09-07 21:37:53 +0600

    dash>NewsUpdate> redid the compo

--- src/components/Dashboard/NewsUpdateItem.js
+++ src/components/Dashboard/NewsUpdateItem.js
@@ -4,31 +4,25 @@ import { Avatar, Box, Typography, Link } from "@material-ui/core";
 
 const BoxContainerStyle = styled(Box)(({ theme }) => ({
   display: "flex",
-  alignItems: "center",
 
   "& .MuiAvatar-root": {
     borderRadius: theme.spacing(1),
+    marginTop: theme.spacing(0.75),
   },
   "& h3": {
     fontWeight: 500,
   },
   "& .MuiTypography-caption": {
     color: theme.palette.text.secondary,
-    //width 200,
-    [theme.breakpoints.up("xs")]: {
-      width: 250,
-    },
-    [theme.breakpoints.up("lg")]: {
-      width: 400,
-    },
-    [theme.breakpoints.up("xl")]: {
-      width: 700,
-    },
   },
   "& .MuiTypography-body2": {
-    flexShrink: 0,
-    paddingRight: theme.spacing(2.5),
+    backgroundColor: theme.palette.gray.lighter,
     color: theme.palette.gray.main,
+    fontWeight: 500,
+    lineHeight: 1,
+    display: "inline-block",
+    borderRadius: 25,
+    padding: "4px 6px",
   },
 }));
 
@@ -37,18 +31,19 @@ const NewsUpdateItem = (props) => {
     <BoxContainerStyle sx={{ my: 1 }}>
       <Avatar src={props.photo} alt={props.title} />
 
-      <Box sx={{ px: 1.5, flex: 1 }}>
+      <Box sx={{ px: 1.5 }}>
         <Link to="/" component={RouterLink} underline="hover" color="inherit">
           <Typography variant="body1" component="h3">
             {props.title}
           </Typography>
         </Link>
-        <Typography variant="caption" component="p" noWrap>
+
+        <Typography variant="caption" component="p">
           {props.subtitle}
         </Typography>
-      </Box>
 
-      <Typography variant="body2">{props.postingTime}</Typography>
+        <Typography variant="body2">{props.postingTime}</Typography>
+      </Box>
     </BoxContainerStyle>
   );
 };
