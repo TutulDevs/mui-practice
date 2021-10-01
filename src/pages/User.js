2ea17b9346c8e3b578570a24126f5a5375cfe499
Tutul <tutulnahid@gmail.com>
2021-10-01 22:57:05 +0600

    User: i think i fixed scroll on the table

--- src/pages/User.js
+++ src/pages/User.js
@@ -1,26 +1,48 @@
-import { Container } from "@material-ui/core";
+import { Container, useMediaQuery } from "@material-ui/core";
+import { styled } from "@material-ui/styles";
 import { Box } from "@mui/system";
 import UserHeader from "../components/User/UserHeader";
 import UserTable from "../components/User/UserTable";
 
+// box style
+const BoxStyle = styled(Box)(({ theme }) => ({
+  margin: `${theme.spacing(4)}px auto`,
+  borderRadius: theme.spacing(2),
+  boxShadow: `rgb(145 158 171 / 24%) 0px 0px 2px 0px, rgb(145 158 171 / 24%) 0px 16px 32px -4px`,
+  overflow: "hidden",
+
+  //border: "2px solid teal",
+}));
+
 const User = () => {
+  // media queries
+  const less400 = useMediaQuery("(max-width:400px)");
+  const less480 = useMediaQuery("(max-width:480px)");
+  const less600 = useMediaQuery("(max-width:600px)");
+  const less768 = useMediaQuery("(max-width:768px)");
+
   return (
-    <Container maxWidth="lg">
+    <Container maxWidth="lg" disableGutters>
       {/* User Header */}
       <UserHeader />
 
       {/* User Table */}
-      <Box
+
+      <BoxStyle
         sx={{
-          width: "100%",
-          my: 8,
-          borderRadius: 4,
-          boxShadow: `rgb(145 158 171 / 24%) 0px 0px 2px 0px, rgb(145 158 171 / 24%) 0px 16px 32px -4px`,
-          overflowX: "hidden",
+          width: less400
+            ? 300
+            : less480
+            ? 360
+            : less600
+            ? 540
+            : less768
+            ? 440
+            : "100%",
         }}
       >
         <UserTable />
-      </Box>
+      </BoxStyle>
     </Container>
   );
 };
