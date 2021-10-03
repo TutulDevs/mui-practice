8d94de6e58a5a90516d1c19d52cd9db81197abdc
Tutul <tutulnahid@gmail.com>
2021-10-03 23:05:03 +0600

    routes: couldnt make the route nesting, shame

--- src/routes.js
+++ src/routes.js
@@ -10,31 +10,68 @@ import Blog from "./pages/Blog";
 import Products from "./pages/Products";
 import User from "./pages/User";
 
-export const PageRoutes = [
+const PageRoutes = [
+  // default
+  {
+    path: "/",
+    component: DashboardLayout,
+    routes: [{ path: "/", component: Dashboard }],
+  },
+  // auth
   {
     path: "/auth",
     component: AuthLayout,
+    routes: [
+      { path: "/auth/login", component: Login },
+      { path: "/auth/register", component: Register },
+    ],
   },
+  // dash
   {
     path: "/dashboard",
     component: DashboardLayout,
     routes: [
-      {
-        path: "/dashboard/app",
-        component: Dashboard,
-      },
-      {
-        path: "/dashboard/user",
-        component: User,
-      },
-      {
-        path: "/dashboard/products",
-        component: Products,
-      },
-      {
-        path: "/dashboard/blog",
-        component: Blog,
-      },
+      { path: "/dashboard/app", component: Dashboard },
+      { path: "/dashboard/user", component: User },
+      { path: "/dashboard/products", component: Products },
+      { path: "/dashboard/blog", component: Blog },
     ],
   },
+  // error
+  { path: "*", component: ErrorPage },
 ];
+
+export default PageRoutes;
+
+/*
+
+Tried this code in the App.js but it didn't work.
+
+///
+import PageRoutes from "./routes";
+console.log(PageRoutes);
+
+const RouteWithSubRoutes = (route) => {
+  return (
+    <Route
+      path={route.path}
+      // pass the sub-routes to nest
+      render={(props) => <route.component {...props} routes={route.routes} />}
+    />
+  );
+};
+
+///
+
+const App = () => {
+  return (
+    <Switch>
+      {PageRoutes.map((route, idx) => (
+        <RouteWithSubRoutes key={idx} {...route} />
+      ))}
+    </Switch>
+  );
+};
+
+
+*/
