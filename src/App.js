11207f2c82dedc3b51a159d43942a3866b241da5
Tutul <tutulnahid@gmail.com>
2021-10-04 22:39:26 +0600

    added different component for IntroModal

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import { useEffect, useState } from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
 import "./App.css";
-import { Link, Modal, Typography } from "@material-ui/core";
-import { Box } from "@mui/system";
 
 // components
 import DashboardLayout from "./layout/DashboardLayout";
@@ -13,6 +11,7 @@ import Register from "./pages/Register";
 import Blog from "./pages/Blog";
 import Products from "./pages/Products";
 import User from "./pages/User";
+import IntroModal from "./components/UI/IntroModal";
 
 const App = () => {
   const [showModal, setShowModal] = useState(false);
@@ -24,56 +23,7 @@ const App = () => {
   return (
     <>
       {/* Modal */}
-      <Modal
-        open={showModal}
-        onClose={toggleShowModal}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box
-          sx={{
-            position: "absolute",
-            top: "50%",
-            left: "50%",
-            transform: "translate(-50%, -50%)",
-            width: "90%",
-            margin: "auto",
-            maxWidth: 450,
-            bgcolor: "#ffffff",
-            outline: "none",
-            borderRadius: 3,
-            minHeight: 150,
-            p: 3,
-            textAlign: "justify",
-          }}
-        >
-          <Typography variant="h6" gutterBottom>
-            Hi there 👋{" "}
-          </Typography>
-
-          <Typography paragraph>
-            Thanks for checking this site. This is a cloned dashboard from
-            Material-UI to practice MUI.
-          </Typography>
-          <Typography paragraph>
-            The main focus of this project was the UI. I know there are some
-            bugs in it. If you're a developer & want to help, please check the{" "}
-            <Link
-              href="https://github.com/TutulDevs/mui-practice"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              repo
-            </Link>
-            .
-          </Typography>
-
-          <Typography paragraph>
-            PS: I'm looking for an entry level job. If you've any opportunity,
-            please knock me. 🙏
-          </Typography>
-        </Box>
-      </Modal>
+      <IntroModal showModal={showModal} onToggle={toggleShowModal} />
 
       {/* Dashboard */}
       <DashboardLayout>
---
+++ src/components/UI/IntroModal.js
@@ -0,0 +1,60 @@
+import { Link, Modal, Typography } from "@material-ui/core";
+import { Box } from "@mui/system";
+
+const IntroModal = ({ showModal, onToggle }) => {
+  return (
+    <Modal
+      open={showModal}
+      onClose={onToggle}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box
+        sx={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          width: "90%",
+          margin: "auto",
+          maxWidth: 450,
+          bgcolor: "#ffffff",
+          outline: "none",
+          borderRadius: 3,
+          minHeight: 150,
+          p: 3,
+          textAlign: "justify",
+          boxShadow: `5px 5px 10px #67787f, -5px -5px 10px #637c66`,
+        }}
+      >
+        <Typography variant="h6" gutterBottom>
+          Hi there 👋{" "}
+        </Typography>
+
+        <Typography paragraph>
+          Thanks for checking this site. This is a cloned dashboard from
+          Material-UI to practice MUI.
+        </Typography>
+        <Typography paragraph>
+          The main focus of this project was the UI. I know there are some bugs
+          in it. If you're a developer & want to help, please check the{" "}
+          <Link
+            href="https://github.com/TutulDevs/mui-practice"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            repo
+          </Link>
+          .
+        </Typography>
+
+        <Typography paragraph>
+          PS: I'm looking for an entry level job. If you've any opportunity,
+          please knock me. 🙏
+        </Typography>
+      </Box>
+    </Modal>
+  );
+};
+
+export default IntroModal;
